330d254fd8035d0bd04d77db29d509da292aaa60
berokwiri150-26 <berokwiri150@gmail.com>
2026-08-27 02:00:59 +0300

    Edited counter.js

--- Documents/leet/javascript/counter.js
+++ Documents/leet/javascript/counter.js
@@ -1,1 +1,11 @@
 // Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+var createCounter = function(n) {
+    let integer = n
+    return function() {
+        return n++;
+};
+};
+
+// This n++ allows the counter function to return the current value of n and then increment it by 1
+// for the next call. The closure created by the inner function allows it to remember the value of n between calls.
