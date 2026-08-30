6f59d379cd40c3156d0a76cfee1d4ad6ca6511dc
berokwiri150-26 <berokwiri150@gmail.com>
2026-08-30 23:50:16 +0300

    Finalised challenge: Error in syntax, values were also not declared at the top of the scope.

--- Documents/leet/javascript/tobeornotbe.js
+++ Documents/leet/javascript/tobeornotbe.js
@@ -1,34 +1,21 @@
-// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
-
-// toBe(val) accepts another value and returns true if the two values === each other.
-//  If they are not equal, it should throw an error "Not Equal".
-// notToBe(val) accepts another value and returns true if the two values !== each other. 
-// If they are equal, it should throw an error "Equal".
-
-// Code :
-/**
- * @param {string} val
- * @return {Object}
- */
 var expect = function(val) {
-  toBe(val);
-  function expect(val) {
+    // toBe(val);
+    function expect(val) {
     return {
         toBe: function(otherVal) {
             if (val === otherVal) {
                 return true;
             } else {
-                throw new Error("Not Equal");
+              throw new Error("Not Equal");
             }
         },
         notToBe: function(otherVal) {
             if (val !== otherVal) {
                 return true;
             } else {
-                throw new Error("Equal")
+              throw new Error("Equal")
             }
         }
     }
-  }
-};
- 
+    };
+}
