31c1ceab8734cfeb414f5bdc6bc0040a3f893780
berokwiri150-26 <berokwiri150@gmail.com>
2026-08-31 10:19:25 +0300

    Notes instructions.

--- Documents/leet/javascript/tobeornotbe.js
+++ Documents/leet/javascript/tobeornotbe.js
@@ -5,23 +5,20 @@
  
 
 var expect = function(val) {
-    // toBe(val);
-    function expect(val) {
     return {
         toBe: function(otherVal) {
             if (val === otherVal) {
                 return true;
             } else {
-              throw new Error("Not Equal");
+                throw new Error("Not Equal");
             }
         },
         notToBe: function(otherVal) {
             if (val !== otherVal) {
                 return true;
             } else {
-              throw new Error("Equal")
+                throw new Error("Equal");
             }
         }
-    }
     };
-}
+};
